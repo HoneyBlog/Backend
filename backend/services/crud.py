82577203c13a426c68533f940e9bbd566f4f57f8
user <eido2488@gmail.com>
2024-07-05 15:07:18 +0300

    encrypted password addition

--- backend/services/crud.py
+++ backend/services/crud.py
@@ -5,31 +5,45 @@ from schemas.schemas import UserCreate, PostCreate
 from uuid import uuid4
 import jwt
 from datetime import datetime, timedelta
+import bcrypt
+import logging
 
 # Replace with your secret key in a real application
 SECRET_KEY = "your-secret-key"
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
 # Users CRUD
 def create_user(db: Session, user: UserCreate):
-    db_user = User(id=str(uuid4()), username=user.username, email=user.email, password=user.password)
+    # Hash the password
+    hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+    db_user = User(id=str(uuid4()), username=user.username, email=user.email, password=hashed_password.decode('utf-8'))
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
-
 def get_users(db: Session):
     return db.query(User).all()
-
 
 def get_user_by_id(db: Session, user_id: str):
     return db.query(User).filter(User.id == user_id).first()
 
-
 def check_login(db: Session, username: str, password: str) -> str:
-    user = db.query(User).filter(User.username == username, User.password == password).first()
+    user = db.query(User).filter(User.username == username).first()
     if user is None:
+        logging.info("User not found")
         raise ValueError("Invalid username or password")
+
+    logging.info(f"Found user: {user.username}, hashed password in DB: {user.password}")
+
+    # Check the password
+    if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+        logging.info("Password check failed")
+        raise ValueError("Invalid username or password")
+
+    logging.info("Password check succeeded")
 
     # Generate JWT token
     token = jwt.encode({
@@ -37,11 +51,9 @@ def check_login(db: Session, username: str, password: str) -> str:
         'exp': datetime.utcnow() + timedelta(hours=1)  # Token expiration time
     }, SECRET_KEY, algorithm='HS256')
 
-    return token , user.id
-
+    return token, user.id
 
 # Posts CRUD
-
 def create_post(db: Session, post: PostCreate):
     db_post = Post(id=str(uuid4()), content=post.content, comments_number=post.comments_number, likes_number=post.likes_number, author_id=post.author_id)
     db.add(db_post)
@@ -49,13 +61,10 @@ def create_post(db: Session, post: PostCreate):
     db.refresh(db_post)
     return db_post
 
-
 def get_posts(db: Session):
     return db.query(Post).all()
 
-
 # Check JWT token : return bool if token is valid
-
 def check_token(token: str) -> bool:
     try:
         jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
