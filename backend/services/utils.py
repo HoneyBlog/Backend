69e7d68ce236731159190e7c101299e880e40534
Adi Levi <adile0194@gmail.com>
2024-07-05 15:37:31 +0300

    updated email validation

--- backend/services/utils.py
+++ backend/services/utils.py
@@ -1,2 +1,18 @@
+import re
+from fastapi import HTTPException
 
-# def CheckLogin(username, password):
+# User Regex
+USERNAME_REGEX = r'^[a-zA-Z0-9._-]{5,20}$'
+PASSWORD_REGEX = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,25}$'
+EMAIL_REGEX = r'^[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,}$'
+
+# User validation
+validation_err = "Invalid username or password"
+
+def user_validaton(user):
+    if not re.match(USERNAME_REGEX, user.username):
+        raise HTTPException(status_code=400, detail=validation_err)
+    if not re.match(PASSWORD_REGEX, user.password):
+        raise HTTPException(status_code=400, detail=validation_err)
+    if not re.match(EMAIL_REGEX, user.email):
+        raise HTTPException(status_code=400, detail=validation_err)
